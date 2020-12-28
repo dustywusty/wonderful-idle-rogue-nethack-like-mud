a0cab4e2367d18939aac5f7f45f1d8894b56ea0e
D <dusty@isperldead.net>
2020-12-27 23:17:19 -0600

    updating main w/ net server example

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,77 @@
-use actix::{Actor, StreamHandler};
-use actix_web::{dev::Server, rt, web, App, Error, HttpRequest, HttpResponse, HttpServer};
-use actix_web_actors::ws;
-use signal_hook::{iterator::Signals, SIGINT};
-use specs::{world::Builder, DispatcherBuilder, World, WorldExt};
-use std::collections::VecDeque;
-use std::sync::{mpsc, Mutex};
-use std::{thread, time};
+use bevy::{
+    app::{ScheduleRunnerPlugin, ScheduleRunnerSettings},
+    ecs::prelude::*,
+    log::*,
+    prelude::*,
+    utils::Duration,
+};
+use bevy_prototype_simple_net::{
+    CreateListener, ListenerConnected, ListenerError, ListenerId, NetPlugin, NetProtocol, Port,
+    SendSocket,
+};
 
-mod components;
-use components::*;
+// ================================================================================================
+// ================================================================================================
 
-mod systems;
-use systems::*;
+const HOST_PORT: Port = 4000;
+const LISTENER_PROTOCOL: NetProtocol = NetProtocol::Tcp;
 
-// ============================================================
-// ============================================================
-
-pub struct GameCommand {}
-
-pub struct GameState {
-    commands: Mutex<VecDeque<GameCommand>>,
-    pub ecs: World,
-}
-
-impl GameState {
-    pub fn foobar() {
-        println!("goobar");
-    }
-}
-
-// ============================================================
-// ============================================================
-
-struct MyWs;
-
-impl Actor for MyWs {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-}
-
-async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(MyWs {}, &req, stream);
-    println!("{:?}", resp);
-    resp
-}
-
-fn run_server(tx: mpsc::Sender<Server>) -> std::io::Result<()> {
-    let mut sys = rt::System::new("test");
-
-    // srv is server controller type, `dev::Server`
-    let srv = HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
-        .bind("127.0.0.1:8080")?
-        .run();
-    // send server controller to main thread
-    let _ = tx.send(srv.clone());
-
-    // run future
-    sys.block_on(srv)
-}
-
-// ============================================================
-// ============================================================
+// ================================================================================================
+// ================================================================================================
 
 fn main() {
-    let mut gs = GameState {
-        ecs: World::new(),
-        commands: Mutex::new(VecDeque::new()),
-    };
+    App::build()
+        .add_resource(ScheduleRunnerSettings::run_loop(Duration::from_millis(500)))
+        .add_plugin(ScheduleRunnerPlugin::default())
+        .add_plugin(NetPlugin)
+        .add_resource(ListenerId::new())
+        .add_startup_system(setup.system())
+        .add_system_to_stage(stage::UPDATE, accept_connections_system.system())
+        .add_system_to_stage(stage::UPDATE, handle_error_system.system())
+        .run();
+}
 
-    let signals = Signals::new(&[SIGINT]);
+// ================================================================================================
+// ================================================================================================
 
-    gs.ecs.register::<Player>();
-    gs.ecs.register::<Name>();
-
-    let (tx, rx) = mpsc::channel();
-
-    println!("START SERVER");
-    thread::spawn(move || {
-        let _ = run_server(tx);
-        for sig in signals.forever() {
-            println!("Received signal {:?}", sig);
-        }
+fn setup(listener_id: Res<ListenerId>, mut listener_create: ResMut<Events<CreateListener>>) {
+    listener_create.send(CreateListener {
+        new_id: *listener_id,
+        port: HOST_PORT,
+        protocol: LISTENER_PROTOCOL,
     });
+    info!(HOST_PORT, "connected to {:?}", "127.0.0.1");
+}
 
-    let srv = rx.recv().unwrap();
+// ================================================================================================
+// ================================================================================================
 
-    println!("WATING 10 SECONDS");
-    thread::sleep(time::Duration::from_secs(10));
+fn accept_connections_system(
+    mut socket_send: ResMut<Events<SendSocket>>,
+    mut state: Local<EventReader<ListenerConnected>>,
+    listener_connected_events: Res<Events<ListenerConnected>>,
+) {
+    // Accept connections and respond with message
+    for listener_connected_event in state.iter(&listener_connected_events) {
+        println!(
+            "Received a new connection from {:?}",
+            listener_connected_event.socket_address
+        );
+        socket_send.send(SendSocket {
+            id: listener_connected_event.socket_id,
+            tx_data: format!("Hello!\n").into_bytes(),
+        })
+    }
+}
 
-    println!("STOPPING SERVER");
-    // init stop server and wait until server gracefully exit
-    rt::System::new("").block_on(srv.stop(true));
-
-    // let _player = gs
-    //     .ecs
-    //     .create_entity()
-    //     .with(Player {})
-    //     .with(Name {
-    //         name: "Dusty".to_string(),
-    //     })
-    //     .build();
-
-    let mut dispatcher = DispatcherBuilder::new()
-        .with(PrintingSystem, "print_sys", &[])
-        .build();
-
-    loop {
-        if gs.commands.lock().unwrap().is_empty() {
-            GameState::foobar();
-        }
-        dispatcher.dispatch(&mut gs.ecs);
-        gs.ecs.maintain();
+fn handle_error_system(
+    mut state: Local<EventReader<ListenerError>>,
+    listener_error_events: Res<Events<ListenerError>>,
+) {
+    for listener_error_event in state.iter(&listener_error_events) {
+        eprintln!(
+            "Listener error (ID: {:?}): \"{:?}\"",
+            listener_error_event.id, listener_error_event.err
+        );
     }
 }
